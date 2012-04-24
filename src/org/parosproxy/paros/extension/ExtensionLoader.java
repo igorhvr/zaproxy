igorhvr/zaproxy

commit c16deeb42b3208a1123112ffdbebfa08d5fd49dd

Bug fixes: Issue 70 - Remember preference for Request [RawView|Tabbed View] Issue 103 - Rework/Refactor HttpBody.class to use a byte oriented buffer instead of StringBuffer Issue 107 - The last intercepted request/response remains in the Break window Issue 180 - Tabular view for GET request Issue 187 - Encoding issues Issue 197 - Decoder cannot process base64 input without line breaks Issue 237 - Bug: Problems with HTTP Panels Issue 260 - Exception when deleting ("Purge (from DB)") "History" tab entries Issue 264 - Exception in "Manual Request Editor"/"Resend" dialogues
Enhancements:
Issue 102 - Save raw response and request, and save body of response and request
Issue 242 - Export node / req/resp via right click

git-svn-id: http://zaproxy.googlecode.com/svn/trunk@1234 0fcac13d-f916-6cb6-7431-acacdca54389

src/org/parosproxy/paros/extension/ExtensionLoader.java
@@ -20,6 +20,9 @@
  */
 // ZAP: 2011/12/14 Support for extension dependencies
 // ZAP: 2012/02/18 Rationalised session handling
+// ZAP: 2012/03/15 Reflected the change in the name of the method optionsChanged of
+//      the class OptionsChangedListener. Changed the method destroyAllExtension() to 
+//      save the configurations of the main http panels and save the configuration file.
 
 package org.parosproxy.paros.extension;
 
@@ -31,6 +34,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.log4j.Logger;
 import org.parosproxy.paros.CommandLine;
 import org.parosproxy.paros.common.AbstractParam;
@@ -70,7 +74,17 @@ public class ExtensionLoader {
         for (int i=0; i<getExtensionCount(); i++) {
             getExtension(i).destroy();
         }
+
+        // ZAP: Save the configurations of the main panels.
+        view.getRequestPanel().saveConfig(model.getOptionsParam().getConfig());
+        view.getResponsePanel().saveConfig(model.getOptionsParam().getConfig());
         
+	    // ZAP: Save the configuration file.
+		try {
+			model.getOptionsParam().getConfig().save();
+		} catch (ConfigurationException e) {
+			logger.error("Error saving config", e);
+		}
     }
     
     public Extension getExtension(int i) {
@@ -143,7 +157,8 @@ public class ExtensionLoader {
                 try {
                     OptionsChangedListener listener = listenerList.get(j);
                     if (listener != null) {
-                        listener.OptionsChanged(options);
+                    	// ZAP: reflected the change in name of the method optionsChanged.
+                        listener.optionsChanged(options);
                     }
                 } catch (Exception e) {
                 	// ZAP: Log the exception

src/org/zaproxy/zap/extension/brk/ExtensionBreak.java
@@ -151,30 +151,6 @@ public class ExtensionBreak extends ExtensionAdaptor implements SessionChangedLi
 	private BreakPointsTableModel getBreakPointsModel() {
 		return (BreakPointsTableModel)this.getBreakPointsPanel().getBreakPoints().getModel();
 	}
-
-	public void sessionChanged(final Session session)  {
-	    if (EventQueue.isDispatchThread()) {
-		    sessionChangedEventHandler(session);
-
-	    } else {
-	        
-	        try {
-	            EventQueue.invokeAndWait(new Runnable() {
-	                public void run() {
-	        		    sessionChangedEventHandler(session);
-	                }
-	            });
-	        } catch (Exception e) {
-	            
-	        }
-	    }
-	}
-	
-	private void sessionChangedEventHandler(Session session) {
-	    getBreakPanel().setMessage(null, true);
-	    getBreakPanel().setMessage(null, false);
-	}
-	
 	
 	private ProxyListenerBreak getProxyListenerBreak() {
         if (proxyListener == null) {
@@ -287,6 +263,36 @@ public class ExtensionBreak extends ExtensionAdaptor implements SessionChangedLi
 	}
 
 	@Override
-	public void sessionAboutToChange(Session session) {
+	public void sessionChanged(Session session)  {
+	}
+
+	@Override
+	public void sessionAboutToChange(final Session session) {
+		if (EventQueue.isDispatchThread()) {
+			sessionChangedEventHandler(session);
+	    } else {
+	        
+	        try {
+	            EventQueue.invokeAndWait(new Runnable() {
+	                public void run() {
+	        		    sessionChangedEventHandler(session);
+	                }
+	            });
+	        } catch (Exception e) {
+	            
+	        }
+	    }
+	}
+	
+	/**
+	 * @param session  
+	 */
+	private void sessionChangedEventHandler(Session session) {
+	    getBreakPanel().reset();
+	}
+	
+	@Override
+	public void destroy() {
+		breakPanel.savePanels();
 	}
 }

commit 3b1e85f4928ee714753a04645e4af7897054473c

Fixed a bug introduced in r1234 that prevented ZAP to shutdown correctly when in daemon mode.
git-svn-id: http://zaproxy.googlecode.com/svn/trunk@1366 0fcac13d-f916-6cb6-7431-acacdca54389

src/org/parosproxy/paros/extension/ExtensionLoader.java
@@ -21,11 +21,12 @@
 // ZAP: 2011/12/14 Support for extension dependencies
 // ZAP: 2012/02/18 Rationalised session handling
 // ZAP: 2012/03/15 Reflected the change in the name of the method optionsChanged of
-//      the class OptionsChangedListener. Changed the method destroyAllExtension() to
-//      save the configurations of the main http panels and save the configuration file.
+// the class OptionsChangedListener. Changed the method destroyAllExtension() to
+// save the configurations of the main http panels and save the configuration file.
 // ZAP: 2012/04/23 Reverted the changes of the method destroyAllExtension(),
-//      now the configurations of the main http panels and the configuration file
-//      are saved in the method Control.shutdown(boolean).
+// now the configurations of the main http panels and the configuration file
+// are saved in the method Control.shutdown(boolean).
+// ZAP: 2012/04/24 Changed the method destroyAllExtension to catch exceptions.
 
 package org.parosproxy.paros.extension;
 
@@ -74,7 +75,12 @@ public class ExtensionLoader {
     
     public void destroyAllExtension() {
         for (int i=0; i<getExtensionCount(); i++) {
-            getExtension(i).destroy();
+            // ZAP: Added try catch block.
+            try {
+                getExtension(i).destroy();
+            } catch (Exception e) {
+               logger.error(e.getMessage(), e);
+            }
         }
         
     }

src/org/zaproxy/zap/extension/brk/ExtensionBreak.java
@@ -313,6 +313,8 @@ public class ExtensionBreak extends ExtensionAdaptor implements SessionChangedLi
 	
 	@Override
 	public void destroy() {
-		breakPanel.savePanels();
+		if (breakPanel != null) {
+			breakPanel.savePanels();
+		}
 	}
 }